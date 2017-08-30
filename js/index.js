2b21202a47d1811f931212e7e55823e7d0a740ba
sasithorn <sasithorneye@gmail.com>
2017-08-30 21:06:00 +0700

    js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function word(n) {
+	document.getElementById("out").innerHTML = document.getElementById("out").innerHTML + n;
+}
+function del(){
+	var o = document.getElementById("out").innerHTML;
+	var x = document.getElementById("out").innerHTML.length;
+	document.getElementById("out").innerHTML = o.substr(0,x-1);
+}
+function empty(){
+	document.getElementById("out").innerHTML = '';
+}
+function bar(n){
+	document.getElementById("out").innerHTML = document.getElementById("out").innerHTML + ' ';
+}
